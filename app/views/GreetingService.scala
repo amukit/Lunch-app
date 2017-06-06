ce1a489768ba5aca0754ae56b80c5e75d1207f95
Abdul Mukit <abdul.mukit@hmrc.gsi.gov.uk>
2017-06-06 14:50:50 +0100

    changessession4

--- app/views/GreetingService.scala
+++ app/views/GreetingService.scala
@@ -1,13 +1,27 @@
 package views
 
+import java.util.Calendar
+
 import com.google.inject.ImplementedBy
 
 
 class RealGreetingService extends GreetingService {
-  def greeting: String = "Alright!"
+  def greeting: String = {
+    val now = Calendar.getInstance()
+    val currentHour = now.get(Calendar.HOUR_OF_DAY)
+    if (currentHour < 12)
+      "Good morning!"
+    else
+      "Good afternoon!"
+
+  }
+
+
 }
+
 
 @ImplementedBy(classOf[RealGreetingService])
 trait GreetingService {
   def greeting: String
+
 }
